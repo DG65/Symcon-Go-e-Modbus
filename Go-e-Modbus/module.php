14e77172e2744f64c7dc4c0378240f24ddde2e55
DG65 <73533984+DG65@users.noreply.github.com>
2020-10-28 23:57:43 +0100

    Update module.php

--- Go-e-Modbus/module.php
+++ Go-e-Modbus/module.php
@@ -100,7 +100,7 @@ class GoeModbus extends Module
             // check register on apply changes in configuration
             if ($this->applied) {
                 try {
-                    $this->modbus->readMultipleRegisters($this->unit_id, (int)30051, 2);
+                    $this->modbus->readMultipleRegisters($this->unit_id, (int)100, 2);
                 } catch (Exception $e) {
                     $this->SetStatus(202);
                     exit(-1);
